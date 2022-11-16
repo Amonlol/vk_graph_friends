0d2ca1ee705c3d8aa9398c6545a09f8ed6aae34b
mefremov <mefremov@ysts.ru>
2022-11-16 17:40:50 +0300

    обновление скрипта

--- vk_friends_graph.py
+++ vk_friends_graph.py
@@ -1,59 +1,48 @@
-!pip install vk_api
+#Для работы скрипта необходимо выполнить в терминале команду ниже
+#py -m pip install vk_api
+#py -m pip install networkx
+
 import vk_api
 import matplotlib.pyplot as plt
 import networkx as nx
 from random import randint
 
-# Логин, пароль для авторизации
-login = 'mail@email.com'
-password = 'password'
+# Исходные данные
+login = input('Введите логин:')
+password = input('Введите пароль:')
+maxFriends = int(input('Введите максимальное количество друзей:'))
 
-# Ограничение на максимальное количество друзей
-MAX_FRIENDS = 5
-
-# Создание подключения к API
+# Попытка подключения к vk api
 vk_session = vk_api.VkApi(login, password)
-
 try:
-    # Получение токена
     vk_session.auth(token_only=True)
 except vk_api.AuthError as error_msg:
     print(error_msg)
 
-# Создание объекта с методами доступа к API
+# Инициализация доступа к vk api
 tools = vk_api.VkTools(vk_session)
-
-# id первоначального юзера
 my_id = 208131632
+friends = tools.get_all('friends.get', 10, {'user_id': my_id, 'count': maxFriends})
 
-# Список друзей юзера
-friends = tools.get_all('friends.get', 10, {'user_id': my_id, 'count': MAX_FRIENDS})
-
-# Граф с друзьями и друзьями друзей
+# Создание графа друзей
 friends_graph = nx.Graph()
-
 c1 = -1
 
 # Добавление друзей и друзей друзей в граф
 for friend_1 in friends['items']:
   c1 = c1+1
-  # Добавить в граф связь: юзер-друг
-  if c1 < MAX_FRIENDS:
+  if c1 < maxFriends:
     friends_graph.add_edge(my_id,friend_1)
     try:
-      # Получить список друзей друга
-      friends2 = tools.get_all('friends.get', 10, {'user_id': friend_1, 'count': MAX_FRIENDS})
+      friends2 = tools.get_all('friends.get', 10, {'user_id': friend_1, 'count': maxFriends})
     except:
-      # Если страница удалена - вывести ошибку
       print(f"user {friend_1} has no friends")
     else:
-      # Если список получен, добавить в граф связи: друг - друзья друга
       c2 = -1
       for friend_2 in friends2['items']:
         c2 = c2+1
-        if c2 < MAX_FRIENDS:
+        if c2 < maxFriends:
           friends_graph.add_edge(friend_1,friend_2)
-
 
 # Близостная центральность
 close_centrality = nx.closeness_centrality(friends_graph)
@@ -72,8 +61,6 @@ eig_centrality = nx.eigenvector_centrality(friends_graph)
 eig_centrality_id = max(eig_centrality.items(), key = lambda k : k[1])
 print("Центральность по собственному значению:")
 print(eig_centrality_id)
-
-#print(friends_graph.edges)
 
 plt.figure(figsize =(30, 15))
 nx.draw_networkx(friends_graph, with_labels = True)

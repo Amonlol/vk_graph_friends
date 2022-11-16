5a115bed6ac472fcc53baf305c4c4dd9df5da6c5
mefremov <mefremov@ysts.ru>
2022-11-16 21:52:11 +0300

    обновление скрипта

--- vk_friends_graph.py
+++ vk_friends_graph.py
@@ -13,54 +13,49 @@ password = input('Введите пароль:')
 maxFriends = int(input('Введите максимальное количество друзей:'))
 
 # Попытка подключения к vk api
-vk_session = vk_api.VkApi(login, password)
+vkConnection = vk_api.VkApi(login, password)
 try:
-    vk_session.auth(token_only=True)
+    vkConnection.auth(token_only=True)
 except vk_api.AuthError as error_msg:
     print(error_msg)
 
 # Инициализация доступа к vk api
-tools = vk_api.VkTools(vk_session)
-my_id = 208131632
-friends = tools.get_all('friends.get', 10, {'user_id': my_id, 'count': maxFriends})
+vk = vk_api.VkTools(vkConnection)
+userId = 208131632
 
 # Создание графа друзей
-friends_graph = nx.Graph()
+graphFriends = nx.Graph()
 c1 = -1
 
 # Добавление друзей и друзей друзей в граф
-for friend_1 in friends['items']:
+myFriends = vk.get_all('friends.get', 10, {'user_id': userId, 'count': maxFriends})
+for myFriend in myFriends['items']:
   c1 = c1+1
   if c1 < maxFriends:
-    friends_graph.add_edge(my_id,friend_1)
+    graphFriends.add_edge(userId,myFriend)
     try:
-      friends2 = tools.get_all('friends.get', 10, {'user_id': friend_1, 'count': maxFriends})
+      relatedFriends = vk.get_all('friends.get', 10, {'user_id': myFriend, 'count': maxFriends})
     except:
-      print(f"user {friend_1} has no friends")
+      print(f"У пользователя {myFriend} нет друзей :(")
     else:
       c2 = -1
-      for friend_2 in friends2['items']:
+      for relatedFriend in relatedFriends['items']:
         c2 = c2+1
         if c2 < maxFriends:
-          friends_graph.add_edge(friend_1,friend_2)
+          graphFriends.add_edge(myFriend,relatedFriend)
 
-# Близостная центральность
-close_centrality = nx.closeness_centrality(friends_graph)
+close_centrality = nx.closeness_centrality(graphFriends)
 close_centrality_id = max(close_centrality.items(), key = lambda k : k[1])
-print("Близостная центральность:")
-print(close_centrality_id)
 
-# Центральность по посредничеству
-bet_centrality = nx.betweenness_centrality(friends_graph, normalized = True, endpoints = False)
+bet_centrality = nx.betweenness_centrality(graphFriends, normalized = True, endpoints = False)
 bet_centrality_id = max(bet_centrality.items(), key = lambda k : k[1])
-print("Центральность по посредничеству:")
-print(bet_centrality_id)
 
-# Центральность по собственному значению
-eig_centrality = nx.eigenvector_centrality(friends_graph)
+eig_centrality = nx.eigenvector_centrality(graphFriends)
 eig_centrality_id = max(eig_centrality.items(), key = lambda k : k[1])
-print("Центральность по собственному значению:")
-print(eig_centrality_id)
+
+print(f"Близостная центральность: {close_centrality_id}\n")
+print(f"Центральность по посредничеству: {bet_centrality_id}\n")
+print(f"Центральность по собственному значению: {eig_centrality_id}\n")
 
 plt.figure(figsize =(30, 15))
-nx.draw_networkx(friends_graph, with_labels = True)
+nx.draw_networkx(graphFriends, with_labels = True)
